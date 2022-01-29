9d4389b133993bab52bd86c79984a98fe4e0d1a8
Eric Vicenti <eric@vicenti.net>
2022-01-29 14:03:23 -0800

    Attempt tamagui install

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,15 +1,22 @@
 import * as React from "react";
 import Head from "next/head";
+import Tamagui from "../tamagui.config";
 
-function MyApp({ Component, pageProps }) {
+function MyApp(props) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <Tamagui.Provider>
+        <ContentInner {...props} />
+      </Tamagui.Provider>
     </>
   );
+}
+
+function ContentInner({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,12 @@
-import { Button } from "ui";
+// import { Button } from "ui";
+import { Button } from "tamagui";
 import { View, Text } from "react-native";
 
 export default function Web() {
   return (
     <View style={{ backgroundColor: "lightblue", flex: 1 }}>
       <Text>Web</Text>
-      <Button onPress={() => {}} />
+      <Button>Press Me!</Button>
     </View>
   );
 }
